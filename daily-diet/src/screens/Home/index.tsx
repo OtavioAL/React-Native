6b6d740f29479856325fc7bdd2dc0d4cdd3fbb57
OtavioAL <otavioauglei@gmail.com>
2024-12-23 13:08:47 -0300

    feat: home screen initial structure

--- daily-diet/src/screens/Home/index.tsx
+++ daily-diet/src/screens/Home/index.tsx
@@ -1,10 +1,62 @@
 import { Text } from "react-native";
-import { Container } from "./styles";
+import { Container, Text as TextStyles } from "./styles";
+import { Header } from "../../components/Header";
+import { StatisticsBanner } from "../../components/StatisticsBanner";
+import { Button } from "../../components/Button";
+import { Plus } from "phosphor-react-native";
+import theme from "../../theme";
+import { ListMeals } from "../../components/ListMeals";
+import { useState } from "react";
+import { MealPlan } from "../../storage/MealsStorageDTO";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Home() {
+  const navigation = useNavigation();
+  const [meals, setMeals] = useState<MealPlan[]>([
+    {
+      date: "2023-08-01",
+      meals: [
+        {
+          id: "1",
+          name: "Cafe da manha ",
+          isOnTheDiet: true,
+          description: "Cafe da manha",
+          hour: "10:00",
+          // date: "2023-08-01",
+        },
+      ],
+    },
+  ]);
+
+  const handleGoToStatistics = () => {
+    navigation.navigate("statistics");
+  };
+
   return (
     <Container>
-      <Text>Home</Text>
+      <Header />
+
+      <StatisticsBanner
+        title={"90,86%"}
+        subtitle="das refeições dentro da dieta"
+        bgColor={"green"}
+        iconPosition="right"
+        // style={{ marginBottom: 40 }}
+        onPress={handleGoToStatistics}
+      />
+
+      <TextStyles>Refeições</TextStyles>
+
+      <Button
+        text="Nova refeição"
+        hasIcon={true}
+        bgColor={theme.COLORS.gray_600}
+        color={theme.COLORS.gray_200}
+        icon={<Plus size={18} color={theme.COLORS.gray_100} />}
+        onPress={() => {}}
+      />
+
+      <ListMeals meals={meals} />
     </Container>
   );
 }
--- daily-diet/src/screens/Home/styles.ts
+++ daily-diet/src/screens/Home/styles.ts
@@ -1,7 +1,19 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { css } from "styled-components";
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.gray_100};
   padding: 24px;
+  gap: 10px;
+`;
+
+export const Text = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.fontFamilys.regular};
+    font-size: ${theme.FONTS.fontSizes.md}px;
+    color: ${theme.COLORS.gray_500};
+  `};
+  margin: 10px 0;
 `;
