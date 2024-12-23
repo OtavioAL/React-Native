11bc51657f043eb76b649862541ed033c75da749
OtavioAL <otavioauglei@gmail.com>
2024-12-23 13:07:14 -0300

    feat: add new screen Statistics

---
+++ daily-diet/src/screens/Statistics/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { Container } from "./styles";
+
+export default function Statistics() {
+  return (
+    <Container>
+      <Text>Estatisticas</Text>
+    </Container>
+  );
+}
