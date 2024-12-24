cd8aa7eab6c0077207d91afa559b343afa6b148c
OtavioAL <otavioauglei@gmail.com>
2024-12-24 00:56:09 -0300

    feat: adding navigation to meal

--- daily-diet/src/components/ListMeals/index.tsx
+++ daily-diet/src/components/ListMeals/index.tsx
@@ -2,12 +2,19 @@ import { SectionList, Text } from "react-native";
 import { MealPlan } from "../../storage/MealsStorageDTO";
 import { Title } from "./styles";
 import { Meal } from "../Meal";
+import { useNavigation } from "@react-navigation/native";
 
 interface IPropsMeals {
   meals: MealPlan[];
 }
 
 export const ListMeals = ({ meals }: IPropsMeals) => {
+  const navigation = useNavigation();
+
+  const handleNavigationMeal = (id: string) => {
+    navigation.navigate("meal", { id });
+  };
+
   return (
     <SectionList
       sections={meals.map((meal) => ({ date: meal.date, data: meal.meals }))}
@@ -27,7 +34,11 @@ export const ListMeals = ({ meals }: IPropsMeals) => {
       }
       renderSectionHeader={({ section }) => <Title>{section.date}</Title>}
       renderItem={({ item }) => (
-        <Meal {...item} isOnTheDiet={item.isOnTheDiet} onPress={() => {}} />
+        <Meal
+          {...item}
+          isOnTheDiet={item.isOnTheDiet}
+          onPress={() => handleNavigationMeal(item.id)}
+        />
       )}
       style={{ marginBottom: 32 }}
     />
