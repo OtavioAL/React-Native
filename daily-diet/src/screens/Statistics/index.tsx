00e5bb55f8ff8732c78ecf7d7099796f3177dd89
OtavioAL <otavioauglei@gmail.com>
2024-12-24 00:57:58 -0300

    feat: add new component Statistics

--- daily-diet/src/screens/Statistics/index.tsx
+++ daily-diet/src/screens/Statistics/index.tsx
@@ -1,10 +1,53 @@
 import { Text } from "react-native";
-import { Container } from "./styles";
+import {
+  Card,
+  Container,
+  ContainerRow,
+  Content,
+  HeaderPage,
+  TextCard,
+  Title,
+  TitleCard,
+} from "./styles";
+import { StatisticsBanner } from "../../components/StatisticsBanner";
+import theme from "../../theme";
 
 export default function Statistics() {
   return (
-    <Container>
-      <Text>Estatisticas</Text>
+    <Container bgColor={theme.COLORS.green_light}>
+      <HeaderPage bgColor={theme.COLORS.green_light}>
+        <StatisticsBanner
+          title={"90,86%"}
+          subtitle="das refeições dentro da dieta"
+          bgColor={"green"}
+          iconPosition="left"
+          // style={{ marginBottom: 40 }}
+          onPress={() => {}}
+        />
+      </HeaderPage>
+      <Content>
+        <Title>Estatísticas gerais</Title>
+
+        <Card bgColor={theme.COLORS.gray_300}>
+          <TitleCard>22</TitleCard>
+          <TextCard>melhor sequência de pratos dentro da dieta</TextCard>
+        </Card>
+        <Card bgColor={theme.COLORS.gray_300}>
+          <TitleCard>109</TitleCard>
+          <TextCard>refeições registradas</TextCard>
+        </Card>
+
+        <ContainerRow>
+          <Card bgColor={theme.COLORS.green_light}>
+            <TitleCard>99</TitleCard>
+            <TextCard>refeições dentro da dieta</TextCard>
+          </Card>
+          <Card bgColor={theme.COLORS.red_light}>
+            <TitleCard>10</TitleCard>
+            <TextCard>refeições fora da dieta</TextCard>
+          </Card>
+        </ContainerRow>
+      </Content>
     </Container>
   );
 }
--- daily-diet/src/screens/Statistics/styles.ts
+++ daily-diet/src/screens/Statistics/styles.ts
@@ -1,7 +1,74 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components/native";
 
-export const Container = styled(SafeAreaView)`
+export interface IPropsColorsStatistics {
+  bgColor?: string;
+}
+
+export const Container = styled(SafeAreaView)<IPropsColorsStatistics>`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.gray_100};
+  background-color: ${({ bgColor }) => bgColor};
+  width: 100%;
+  /* padding: 24px; */
+`;
+
+export const HeaderPage = styled.View<IPropsColorsStatistics>`
+  width: 100%;
+  padding: 10px;
+  background-color: ${({ bgColor }) => bgColor};
+  min-height: 170px;
+  align-items: center;
+  justify-content: center;
+`;
+export const Content = styled.View`
+  flex: 1;
+  align-items: center;
+  padding: 24px;
+  background-color: ${({ theme }) => theme.COLORS.gray_100};
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+  gap: 10px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.FONTS.fontFamilys.bold};
+  font-size: ${({ theme }) => theme.FONTS.fontSizes.md}px;
+  color: ${({ theme }) => theme.COLORS.gray_700};
+  margin-bottom: 5px;
+  margin-top: 20px;
+`;
+
+export const Card = styled.View<IPropsColorsStatistics>`
+  background-color: ${({ bgColor }) => bgColor};
+  flex: 1;
+  width: 100%;
+  max-height: 90px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  padding: 12px;
+  border-radius: 6px;
+  margin-bottom: 8px;
+`;
+
+export const TitleCard = styled.Text`
+  font-family: ${({ theme }) => theme.FONTS.fontFamilys.bold};
+  font-size: ${({ theme }) => theme.FONTS.fontSizes.xl}px;
+  color: ${({ theme }) => theme.COLORS.gray_700};
+`;
+
+export const TextCard = styled.Text`
+  font-family: ${({ theme }) => theme.FONTS.fontFamilys.regular};
+  font-size: ${({ theme }) => theme.FONTS.fontSizes.md}px;
+  color: ${({ theme }) => theme.COLORS.gray_600};
+  text-align: center;
+`;
+
+export const ContainerRow = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 10px;
 `;
