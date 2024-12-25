6b2cd595cb188a987932a6ee61bdd652f26d98ff
OtavioAL <otavioauglei@gmail.com>
2024-12-24 22:18:43 -0300

    feat: add integration RegisterMeal

--- daily-diet/src/screens/RegisterMeal/index.tsx
+++ daily-diet/src/screens/RegisterMeal/index.tsx
@@ -1,18 +1,57 @@
-import { TextInput } from "react-native";
 import { Button } from "../../components/Button";
-import { Input } from "../../components/Input";
 import theme from "../../theme";
-import {
-  ContainerButton,
-  ContainerRow,
-  IndicatorStatus,
-  OptionsSelect,
-  Text,
-} from "./styles";
+import { ContainerButton } from "./styles";
 import { ContentDefault } from "../../components/ContentDefault";
 import { FormMeal } from "../../components/FormMeal";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { registerMealValidator } from "../../validators/registerMealValidator";
+import { mealCreate } from "../../storage/meal/mealCreate";
+import { useNavigation } from "@react-navigation/native";
 
 export default function RegisterMeal() {
+  const navigation = useNavigation();
+  const {
+    control,
+    setValue,
+    watch,
+    getValues,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registerMealValidator),
+    defaultValues: {
+      name: "",
+      description: "",
+      date: "",
+      hour: "",
+      isOnTheDiet: undefined,
+    },
+  });
+
+  const handleCreateMeal = async () => {
+    try {
+      const mealFormated = {
+        id: Math.random().toString(36).substring(2, 9),
+        name: watch("name"),
+        description: watch("description"),
+        date: watch("date"),
+        hour: watch("hour"),
+        isOnTheDiet: watch("isOnTheDiet"),
+      };
+
+      await registerMealValidator.validate(mealFormated, { abortEarly: true });
+
+      await mealCreate(mealFormated);
+
+      navigation.navigate("completingMealRegistration", {
+        isOnTheDiet: watch("isOnTheDiet"),
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   return (
     <>
       <ContentDefault
@@ -20,7 +59,14 @@ export default function RegisterMeal() {
         backgroundColor={theme.COLORS.gray_300}
       >
         <>
-          <FormMeal />
+          <FormMeal
+            control={control}
+            setValue={setValue}
+            watch={watch}
+            getValue={getValues}
+            register={register}
+            errors={errors}
+          />
 
           <ContainerButton>
             <Button
@@ -28,7 +74,7 @@ export default function RegisterMeal() {
               hasIcon={false}
               bgColor={theme.COLORS.gray_600}
               color={theme.COLORS.gray_200}
-              onPress={() => {}}
+              onPress={() => handleCreateMeal()}
             />
           </ContainerButton>
         </>
