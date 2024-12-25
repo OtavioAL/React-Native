3352e5053cf24b70ff24d29b84ceabd39740acc1
OtavioAL <otavioauglei@gmail.com>
2024-12-24 22:14:11 -0300

    feat: add new routes

--- daily-diet/src/@types/navigation.d.ts
+++ daily-diet/src/@types/navigation.d.ts
@@ -2,10 +2,17 @@ export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined;
-      statistics: undefined;
+      statistics: {
+        listMeals;
+        numberMealsWithoutDiet;
+        numberMealsOutsideDiet;
+        percentageMealsOnDiet;
+        totalMeals;
+      };
       registerMeal: undefined;
       completingMealRegistration: { isOnTheDiet: boolean };
       meal: { id: string };
+      editMeal: { id: string };
     }
   }
 }
