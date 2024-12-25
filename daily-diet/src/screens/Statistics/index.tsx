d09936b79ec3e5d9ff46af05a3baaf4380b44c6e
OtavioAL <otavioauglei@gmail.com>
2024-12-24 22:19:00 -0300

    feat: add integration Statistics

--- daily-diet/src/screens/Statistics/index.tsx
+++ daily-diet/src/screens/Statistics/index.tsx
@@ -11,39 +11,92 @@ import {
 } from "./styles";
 import { StatisticsBanner } from "../../components/StatisticsBanner";
 import theme from "../../theme";
+import { MealPlan } from "../../storage/MealsStorageDTO";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+export interface IPropsStatistics {
+  listMeals: MealPlan[];
+  numberMealsWithoutDiet: number;
+  numberMealsOutsideDiet: number;
+  percentageMealsOnDiet: number;
+  totalMeals: number;
+}
 
 export default function Statistics() {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const {
+    listMeals,
+    numberMealsOutsideDiet,
+    numberMealsWithoutDiet,
+    percentageMealsOnDiet,
+    totalMeals,
+  } = route.params as IPropsStatistics;
+
+  const handleGoToHome = () => {
+    navigation.goBack();
+  };
+
+  const bestSequence = () => {
+    let best = 0;
+    let current = 0;
+
+    listMeals.forEach((meal) => {
+      meal.meals.forEach((mealOfDay) => {
+        if (mealOfDay.isOnTheDiet) {
+          current++;
+          best = Math.max(best, current);
+        } else {
+          current = 0;
+        }
+      });
+    });
+
+    return best;
+  };
+
   return (
-    <Container bgColor={theme.COLORS.green_light}>
-      <HeaderPage bgColor={theme.COLORS.green_light}>
+    <Container
+      bgColor={
+        percentageMealsOnDiet >= 60
+          ? theme.COLORS.green_light
+          : theme.COLORS.red_light
+      }
+    >
+      <HeaderPage
+        bgColor={
+          percentageMealsOnDiet >= 60
+            ? theme.COLORS.green_light
+            : theme.COLORS.red_light
+        }
+      >
         <StatisticsBanner
-          title={"90,86%"}
+          title={`${percentageMealsOnDiet.toFixed(2)}%`}
           subtitle="das refeições dentro da dieta"
-          bgColor={"green"}
+          bgColor={percentageMealsOnDiet >= 60 ? "green" : "red"}
           iconPosition="left"
-          // style={{ marginBottom: 40 }}
-          onPress={() => {}}
+          onPress={handleGoToHome}
         />
       </HeaderPage>
       <Content>
         <Title>Estatísticas gerais</Title>
 
         <Card bgColor={theme.COLORS.gray_300}>
-          <TitleCard>22</TitleCard>
+          <TitleCard>{bestSequence()}</TitleCard>
           <TextCard>melhor sequência de pratos dentro da dieta</TextCard>
         </Card>
         <Card bgColor={theme.COLORS.gray_300}>
-          <TitleCard>109</TitleCard>
+          <TitleCard>{totalMeals ?? 0}</TitleCard>
           <TextCard>refeições registradas</TextCard>
         </Card>
 
         <ContainerRow>
           <Card bgColor={theme.COLORS.green_light}>
-            <TitleCard>99</TitleCard>
+            <TitleCard>{numberMealsWithoutDiet ?? 0}</TitleCard>
             <TextCard>refeições dentro da dieta</TextCard>
           </Card>
           <Card bgColor={theme.COLORS.red_light}>
-            <TitleCard>10</TitleCard>
+            <TitleCard>{numberMealsOutsideDiet ?? 0}</TitleCard>
             <TextCard>refeições fora da dieta</TextCard>
           </Card>
         </ContainerRow>
