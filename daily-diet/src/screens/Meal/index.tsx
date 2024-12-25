22b8f87e9a8cc9fe67090bb5e5b96d16054cbf0e
OtavioAL <otavioauglei@gmail.com>
2024-12-24 22:18:25 -0300

    feat: add integration Meal

--- daily-diet/src/screens/Meal/index.tsx
+++ daily-diet/src/screens/Meal/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { Button } from "../../components/Button";
 import { ContentDefault } from "../../components/ContentDefault";
 import theme from "../../theme";
@@ -10,16 +11,60 @@ import {
   Title,
 } from "./styles";
 import { PencilLine, Trash } from "phosphor-react-native";
+import { useEffect, useState } from "react";
+import { mealsGet } from "../../storage/meal/mealsGet";
+import { MealDTO } from "../../storage/MealsStorageDTO";
+import { mealRemove } from "../../storage/meal/mealRemove";
+import { Alert } from "react-native";
 
 interface IPropsMeal {
   id: string;
-  hour: string;
-  name: string;
-  date: string;
-  description: string;
+  // hour: string;
+  // name: string;
+  // date: string;
+  // description: string;
 }
 
 export default function Meal() {
+  const route = useRoute();
+  const { id } = route.params as IPropsMeal;
+  const [meal, setMeal] = useState<MealDTO>();
+  const navigation = useNavigation();
+
+  const fetchMeal = async () => {
+    try {
+      const meal = await mealsGet(id);
+      console.log("meal", meal);
+      setMeal(meal);
+    } catch (error) {
+      console.error("Failed to fetch meal", error);
+    }
+  };
+
+  const mealsRemove = async () => {
+    try {
+      await mealRemove(id);
+      navigation.navigate("home");
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  async function handleMealRemove() {
+    Alert.alert("Remover", "Deseja remover a refeição?", [
+      { text: "Não", style: "cancel" },
+      { text: "Sim", onPress: () => mealsRemove() },
+    ]);
+  }
+
+  const handleGoToEdit = () => {
+    navigation.navigate("editMeal", { id });
+  };
+
+  useEffect(() => {
+    if (id) fetchMeal();
+  }, [id]);
+
   return (
     <>
       <ContentDefault
@@ -27,19 +72,19 @@ export default function Meal() {
         title="Refeição"
       >
         <Container>
-          <Title>Sanduiche</Title>
+          <Title>{meal?.name}</Title>
 
-          <Text>
-            Sanduíche de pão integral com atum e salada de alface e tomate
-          </Text>
+          <Text>{meal?.description}</Text>
 
           <TextDate>Data e hora</TextDate>
 
-          <Text>12/08/2022 às 16:00</Text>
+          <Text>{`${meal?.date} às ${meal?.hour}`}</Text>
 
-          <OptionsSelect isSelected>
-            <IndicatorStatus isGreenOption />
-            <Text>dentro da dieta</Text>
+          <OptionsSelect isSelected={meal?.isOnTheDiet}>
+            <IndicatorStatus isGreenOption={meal?.isOnTheDiet} />
+            <Text>
+              {meal?.isOnTheDiet ? "dentro da dieta" : "fora da dieta"}
+            </Text>
           </OptionsSelect>
         </Container>
 
@@ -49,7 +94,7 @@ export default function Meal() {
           bgColor={theme.COLORS.gray_600}
           color={theme.COLORS.gray_200}
           icon={<PencilLine size={18} color={theme.COLORS.gray_100} />}
-          onPress={() => {}}
+          onPress={handleGoToEdit}
         />
         <Button
           text="Excluir refeição"
@@ -57,7 +102,7 @@ export default function Meal() {
           bgColor={"transparent"}
           color={theme.COLORS.gray_700}
           icon={<Trash size={18} color={theme.COLORS.gray_700} />}
-          onPress={() => {}}
+          onPress={handleMealRemove}
           type="secondary"
         />
       </ContentDefault>
