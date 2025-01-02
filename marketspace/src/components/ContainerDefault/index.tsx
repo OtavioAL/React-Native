40db04d7555c0f4ddf13c1e1cd6e382e7c3091b8
OtavioAL <otavioauglei@gmail.com>
2025-01-02 17:19:17 -0300

    feat: add new component ContainerDefault

---
+++ marketspace/src/components/ContainerDefault/index.tsx
@@ -0,0 +1,15 @@
+import { ScrollView } from "@gluestack-ui/themed";
+import { SafeAreaView } from "@gluestack-ui/themed";
+
+export function ContainerDefault({ children }: { children: React.ReactNode }) {
+  return (
+    <SafeAreaView flex={1} pt={6}>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        showsVerticalScrollIndicator={false}
+      >
+        {children}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
