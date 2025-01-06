4db30d1abe97b32bee259c80a239673e8db239a3
OtavioAL <otavioauglei@gmail.com>
2025-01-05 23:56:52 -0300

    feat: integrating private route display

--- marketspace/src/routes/index.tsx
+++ marketspace/src/routes/index.tsx
@@ -2,15 +2,23 @@ import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 import { Box } from "@gluestack-ui/themed";
+import { useAuth } from "@hooks/useAuth";
+import { Loading } from "@components/Loading";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const theme = DefaultTheme;
-  theme.colors.background = gluestackUIConfig.tokens.colors.gray700;
+  theme.colors.background = gluestackUIConfig.tokens.colors.gray200;
+  const { user, isLoadingUserStorageData } = useAuth();
+
+  if (isLoadingUserStorageData) {
+    return <Loading />;
+  }
 
   return (
     <Box flex={1} bg="$gray200">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
