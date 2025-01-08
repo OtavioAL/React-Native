a69c90bc2bb7c110129107d6e6d854e0b215ce37
OtavioAL <otavioauglei@gmail.com>
2025-01-08 01:35:58 -0300

    feat: adding page integration DetailsMyPage

--- marketspace/src/modules/DetailsMyAdPage/components/Footer/index.tsx
+++ marketspace/src/modules/DetailsMyAdPage/components/Footer/index.tsx
@@ -1,7 +1,102 @@
 import { Button } from "@components/Button";
-import { VStack } from "@gluestack-ui/themed";
+import { ToastMessage } from "@components/ToastMessage";
+import { ProductsDTO } from "@dtos/ProductsDTO";
+import { useToast, VStack } from "@gluestack-ui/themed";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import { api } from "@services/api";
+import { AppError } from "@utils/AppError";
+import { Dispatch, SetStateAction, useState } from "react";
 
-export const Footer = () => {
+interface IProps {
+  id: string;
+  product?: ProductsDTO;
+  setProduct: Dispatch<SetStateAction<ProductsDTO | undefined>>;
+}
+export const Footer = ({ id, product, setProduct }: IProps) => {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const toast = useToast();
+  const [isChangingVisibilityLoading, setIsChangingVisibilityLoading] =
+    useState<boolean>(false);
+
+  const [isLoadingDeleting, setIsDeletingLoading] = useState<boolean>(false);
+
+  const handleChangeAdVisibility = async () => {
+    if (!product) return;
+    try {
+      setIsChangingVisibilityLoading(true);
+      const data = await api.patch(`products/${id}`, {
+        is_active: !product.is_active,
+      });
+
+      if (product) {
+        setProduct((state) => {
+          return {
+            ...state,
+            is_active: state ? !state.is_active : false,
+          };
+        });
+      }
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível deletar. Tente Novamente!";
+
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <ToastMessage
+            action="error"
+            id={id}
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      });
+    } finally {
+      setIsChangingVisibilityLoading(false);
+    }
+  };
+
+  const handleDeleteAd = async () => {
+    try {
+      setIsDeletingLoading(true);
+      await api.delete(`products/${id}`);
+
+      navigation.navigate("myads");
+
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <ToastMessage
+            action="success"
+            id={id}
+            title={"Anuncio deletado com sucesso!"}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível deletar. Tente Novamente!";
+
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <ToastMessage
+            action="error"
+            id={id}
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      });
+    }
+  };
+
   return (
     <VStack
       w={"$full"}
@@ -12,16 +107,18 @@ export const Footer = () => {
       bg="$gray100"
     >
       <Button
-        title="Desativar anúncior"
-        onPress={() => {}}
-        type="black"
+        title={product?.is_active ? "Desativar anúncio" : "Reativar anúncio"}
+        onPress={handleChangeAdVisibility}
+        type={product?.is_active ? "black" : "primary"}
         w={"$full"}
+        isLoading={isChangingVisibilityLoading}
       />
       <Button
         title="Excluir anúncio"
-        onPress={() => {}}
+        onPress={handleDeleteAd}
         type="secondary"
         w={"$full"}
+        isLoading={isLoadingDeleting}
       />
     </VStack>
   );
--- marketspace/src/modules/DetailsMyAdPage/index.tsx
+++ marketspace/src/modules/DetailsMyAdPage/index.tsx
@@ -1,23 +1,68 @@
 import { ContainerDefault } from "@components/ContainerDefault";
-import { Box } from "@gluestack-ui/themed";
+import { Box, useToast } from "@gluestack-ui/themed";
 import { Footer } from "./components/Footer";
 import { HeaderPages } from "@components/HeaderPages";
 import { Button as GluestackButton } from "@gluestack-ui/themed";
 import { ArrowLeft, PencilLine } from "phosphor-react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 import { ContentAd } from "@components/ContentAd";
+import { useCallback, useEffect, useState } from "react";
+import { ProductsDTO } from "@dtos/ProductsDTO";
+import { ToastMessage } from "@components/ToastMessage";
+import { AppError } from "@utils/AppError";
+import { api } from "@services/api";
+import { Center } from "@gluestack-ui/themed";
+import { Loading } from "@components/Loading";
 
 export const DetailsMyAdPage = () => {
   const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const [products, setProducts] = useState<ProductsDTO>();
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+  const route = useRoute();
+  const { id } = route.params as any;
 
   const handleGoBack = () => {
     navigation.goBack();
   };
 
   const handleGoToEditAd = () => {
-    navigation.navigate("adCreated");
+    navigation.navigate("adEdit", {
+      id,
+    });
   };
+
+  const loadProducts = async (id: string) => {
+    setIsLoading(true);
+    try {
+      const productsData = await api.get(`/products/${id}`);
+
+      setProducts(productsData.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta. Tente novamente mais tarde.";
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <ToastMessage
+            action="error"
+            id={id}
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (id) loadProducts(id);
+  }, [id]);
 
   return (
     <ContainerDefault>
@@ -28,6 +73,8 @@ export const DetailsMyAdPage = () => {
         alignItems="flex-start"
         justifyContent="flex-start"
         backgroundColor="$gray200"
+        w={"$full"}
+        h={"$full"}
       >
         <HeaderPages
           isButtonLeft
@@ -60,9 +107,15 @@ export const DetailsMyAdPage = () => {
           isButtonRight
         />
 
-        <ContentAd />
+        {isLoading ? (
+          <Center w={"$full"} h={"$full"}>
+            <Loading />
+          </Center>
+        ) : (
+          <ContentAd product={products} />
+        )}
 
-        <Footer />
+        <Footer id={id} product={products} setProduct={setProducts} />
       </Box>
     </ContainerDefault>
   );
