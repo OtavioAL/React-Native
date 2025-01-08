8d1dd380c53e8e85b115f596f4e4d5d7f0e4a4af
OtavioAL <otavioauglei@gmail.com>
2025-01-08 01:33:00 -0300

    feat: creation ProductsDTO

---
+++ marketspace/src/dtos/ProductsDTO.ts
@@ -0,0 +1,14 @@
+export type ProductsDTO = {
+  accept_trade: boolean;
+  created_at: string;
+  description: string;
+  id: string;
+  is_active: boolean;
+  is_new: boolean;
+  name: string;
+  payment_methods: any[];
+  price: number;
+  product_images: any[];
+  updated_at: string;
+  user_id: string;
+};
