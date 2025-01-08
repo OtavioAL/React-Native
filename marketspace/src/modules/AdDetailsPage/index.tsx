925323dce8694a0af89430b8f51174a2165466f3
OtavioAL <otavioauglei@gmail.com>
2025-01-08 01:33:53 -0300

    feat: add integrating page AdDetailsPage

--- marketspace/src/modules/AdDetailsPage/index.tsx
+++ marketspace/src/modules/AdDetailsPage/index.tsx
@@ -1,19 +1,61 @@
 import { ContainerDefault } from "@components/ContainerDefault";
-import { Box } from "@gluestack-ui/themed";
+import { Box, Center, useToast } from "@gluestack-ui/themed";
 import { Footer } from "./components/Footer";
 import { HeaderPages } from "@components/HeaderPages";
 import { Button as GluestackButton } from "@gluestack-ui/themed";
 import { ArrowLeft } from "phosphor-react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 import { ContentAd } from "@components/ContentAd";
+import { useEffect, useState } from "react";
+import { ProductsDTO } from "@dtos/ProductsDTO";
+import { api } from "@services/api";
+import { AppError } from "@utils/AppError";
+import { ToastMessage } from "@components/ToastMessage";
+import { Loading } from "@components/Loading";
 
 export const AdDetailsPage = () => {
   const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const [products, setProducts] = useState<ProductsDTO>();
+  const [isLoading, setIsLoading] = useState(false);
+  const route = useRoute();
+  const { adId } = route.params as any;
+  const toast = useToast();
 
   const handleGoBack = () => {
     navigation.goBack();
   };
+
+  const loadProducts = async (adId: string) => {
+    setIsLoading(true);
+    try {
+      const productsData = await api.get(`/products/${adId}`);
+
+      setProducts(productsData.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta. Tente novamente mais tarde.";
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <ToastMessage
+            action="error"
+            id={id}
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (adId) loadProducts(adId);
+  }, [adId]);
 
   return (
     <ContainerDefault>
@@ -42,7 +84,13 @@ export const AdDetailsPage = () => {
           }}
         />
 
-        <ContentAd />
+        {isLoading ? (
+          <Center w={"$full"} h={"$full"}>
+            <Loading />
+          </Center>
+        ) : (
+          <ContentAd product={products} />
+        )}
 
         <Footer />
       </Box>
