6e0c6ea208bb2336935065c3bfeeaa6a824465b6
OtavioAL <otavioauglei@gmail.com>
2025-01-08 01:28:41 -0300

    feat: adding integration in the component AdvertiserUser

--- marketspace/src/components/ContentAd/components/AdvertiserUser/index.tsx
+++ marketspace/src/components/ContentAd/components/AdvertiserUser/index.tsx
@@ -2,19 +2,24 @@ import { UserPhoto } from "@components/UserPhoto";
 import { HStack, Text } from "@gluestack-ui/themed";
 import defaulUserPhotoImg from "../../../../../assets/imageUserDefault.png";
 
-export const AdvertiserUser = () => {
+interface IProps {
+  name: string;
+  image?: string;
+}
+
+export const AdvertiserUser = ({ name, image }: IProps) => {
   return (
     <HStack w={"$full"} p={"$5"} gap={"$2"} alignItems="center">
       <UserPhoto
         width={30}
         height={30}
-        source={defaulUserPhotoImg}
+        source={image ?? defaulUserPhotoImg}
         size="md"
         alt="Imagem do usuário"
       />
 
       <Text fontFamily="$body" fontSize={"$md"}>
-        Makenna Baptista
+        {name ?? ""}
       </Text>
     </HStack>
   );
