c104e928c1931fb16dae49fbb992cc85e8c94dcd
OtavioAL <otavioauglei@gmail.com>
2025-01-08 01:28:24 -0300

    feat: adding integration in the component ContentAd

--- marketspace/src/components/ContentAd/index.tsx
+++ marketspace/src/components/ContentAd/index.tsx
@@ -3,17 +3,50 @@ import { ListImages } from "./components/ListImages";
 import { AdvertiserUser } from "./components/AdvertiserUser";
 import { Description } from "./components/Description";
 import { OptionsAds } from "./components/OptionsAds";
+import { useRoute } from "@react-navigation/native";
+import { RouteParamsAdPreview } from "@components/interfaces";
+import { ProductsDTO } from "@dtos/ProductsDTO";
 
-export function ContentAd() {
+interface IProps {
+  product?: ProductsDTO;
+}
+
+export function ContentAd({ product }: IProps) {
+  const route = useRoute();
+  const {
+    title,
+    description,
+    value,
+    listImages,
+    paymentMethods,
+    isNew,
+    isAcceptExchange,
+    imageUser,
+    nameUser,
+  } = route.params as RouteParamsAdPreview;
+
   return (
     <VStack w={"$full"}>
-      <ListImages />
+      <ListImages images={listImages ?? product?.product_images} />
 
-      <AdvertiserUser />
+      {nameUser?.length && imageUser && (
+        <AdvertiserUser name={nameUser ?? ""} image={imageUser} />
+      )}
 
-      <Description />
+      <Description
+        title={title ?? product?.name ?? ""}
+        description={description ?? product?.description ?? ""}
+        value={value ?? `${product?.price}` ?? ""}
+        isNew={isNew ?? product?.is_new}
+      />
 
-      <OptionsAds />
+      <OptionsAds
+        isAcceptExchange={isAcceptExchange ?? product?.accept_trade}
+        paymentMethods={
+          paymentMethods ??
+          product?.payment_methods?.map((method) => method?.key)
+        }
+      />
     </VStack>
   );
 }
