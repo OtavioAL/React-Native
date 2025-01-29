ad5c5c5aeea0c0bd407fa35a3098eb4d1c55cc06
OtavioAL <otavioauglei@gmail.com>
2025-01-29 00:20:06 -0300

    feat: adding mock cafe data

---
+++ coffe-delivery/src/utils/coffes.ts
@@ -0,0 +1,144 @@
+export const OUR_COFFEES_BANNER = [
+  {
+    id: 4,
+    title: "Latte",
+    tag: "tradicional",
+    image: "latte",
+    description: "Café expresso com o dobro de leite e espuma cremosa",
+    value: 9.9,
+  },
+  {
+    id: 7,
+    tag: "doce",
+    title: "Mochaccino",
+    image: "mochaccino",
+    description: "Café expresso com calda de chocolate, pouco leite e espuma",
+    value: 9.9,
+  },
+  {
+    id: 12,
+    tag: "especial",
+    title: "Irlandês",
+    image: "irlandes",
+    description: "Bebida a base de café, uísque irlandês, açúcar e chantilly",
+    value: 9.9,
+  },
+];
+
+export const OUR_COFFEES = [
+  {
+    title: "tradicionais",
+    data: [
+      {
+        id: 1,
+        title: "Expresso Tradicional",
+        tag: "tradicional",
+        image: "expresso",
+        description: "O tradicional café feito com água quente e grãos moídos",
+        value: 9.9,
+      },
+      {
+        id: 2,
+        title: "Expresso Americano",
+        tag: "tradicional",
+        image: "americano",
+        description: "Expresso diluído, menos intenso que o tradicional",
+        value: 9.9,
+      },
+      {
+        id: 3,
+        title: "Expresso Cremoso",
+        tag: "tradicional",
+        image: "cremoso",
+        description: "Café expresso tradicional com espuma cremosa",
+        value: 9.9,
+      },
+      {
+        id: 4,
+        title: "Latte",
+        tag: "tradicional",
+        image: "latte",
+        description: "Café expresso com o dobro de leite e espuma cremosa",
+        value: 9.9,
+      },
+      {
+        id: 5,
+        title: "Expresso Gelado",
+        tag: "tradicional",
+        image: "gelado",
+        description: "Bebida preparada com café expresso e cubos de gelo",
+        value: 9.9,
+      },
+    ],
+  },
+  {
+    title: "doces",
+    data: [
+      {
+        id: 6,
+        tag: "doce",
+        title: "Capuccino",
+        image: "capuccino",
+        description: "Bebida com canela feita de doses de café, leite e espuma",
+        value: 9.9,
+      },
+      {
+        id: 7,
+        tag: "doce",
+        title: "mochaccino",
+        image: "mochaccino",
+        description:
+          "Café expresso com calda de chocolate, pouco leite e espuma",
+        value: 9.9,
+      },
+      {
+        id: 8,
+        tag: "doce",
+        title: "Chocolate Quente",
+        image: "chocolate-quente",
+        description:
+          "Bebida feita com chocolate dissolvido no leite quente e café",
+        value: 9.9,
+      },
+    ],
+  },
+  {
+    title: "especiais",
+    data: [
+      {
+        id: 9,
+        tag: "especial",
+        title: "Cubano",
+        image: "cubano",
+        description:
+          "Drink gelado de café expresso com rum, creme de leite e hortelã",
+        value: 9.9,
+      },
+      {
+        id: 10,
+        tag: "especial",
+        title: "Havaiano",
+        image: "havaiano",
+        description: "Bebida adocicada preparada com café e leite de coco",
+        value: 9.9,
+      },
+      {
+        id: 11,
+        title: "Árabe",
+        tag: "especial",
+        image: "arabe",
+        description: "Bebida preparada com grãos de café árabe e especiarias",
+        value: 9.9,
+      },
+      {
+        id: 12,
+        tag: "especial",
+        title: "Irlandês",
+        image: "irlandes",
+        description:
+          "Bebida a base de café, uísque irlandês, açúcar e chantilly",
+        value: 9.9,
+      },
+    ],
+  },
+];
